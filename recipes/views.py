c7da60324787c5dd524d7f51ddb3c941409d1492
Antonio Lindo <antoniolvitall@gmail.com>
2023-01-28 10:07:05 -0300

    organizing urls

--- recipes/views.py
+++ recipes/views.py
@@ -1,3 +1,14 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+
+def home(request):
+    return HttpResponse('home 2')
+
+
+def contato(request):
+    return HttpResponse('contato2')
+
+
+def sobre(request):
+    return HttpResponse('sobre2')
