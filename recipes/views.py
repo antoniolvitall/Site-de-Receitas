5e4fc8303003f0f98d823a187604eddc7e570fc8
Antonio Lindo <antoniolvitall@gmail.com>
2023-02-03 19:19:34 -0300

    creating new urls

--- recipes/views.py
+++ recipes/views.py
@@ -5,3 +5,9 @@ def home(request):
     return render(request, 'recipes/pages/home.html', context={
         'name': 'Antonio Lindo'
     })
+
+
+def recipe(request, id):
+    return render(request, 'recipes/pages/home.html', context={
+        'name': 'Antonio Lindo'
+    })
